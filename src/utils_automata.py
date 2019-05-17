3fc80909a6b3b096179967d341e16e61ac57c12b
Thibault Lepez <thibault.lepez@efrei.net>
2019-05-17 15:06:21 +0200

    correct utlis

--- src/utils_automata.py
+++ src/utils_automata.py
@@ -1,12 +1,12 @@
 from Automata import *
 
-def is_synchronous(fa:Automata):
+def is_asynchronous(fa:Automata):
     for state in fa.states:
         for letter in fa.states[state]:
             if letter is "*":
                 print(str(state) + " state has an * transition")
-                return False
-    return True
+                return True
+    return False
 
 def is_deterministic(fa:Automata):
     if len(fa.initialStates) is not 1:
@@ -15,8 +15,11 @@ def is_deterministic(fa:Automata):
         print("A asynchronous can't be deterministic")
         return False
     else:
-        for letter in fa.symboles:
-            for state in fa.states:
-                if len(state[letter]) > 1:
+        for state in fa.states:
+            for char in fa.states[state]:
+                if len(fa.states[state][char]) > 1:
                     return False
         return True
+
+def is_complete(fa:Automata):
+    pass
